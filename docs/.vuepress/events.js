fe102311b540fb3c7bc3def9e85836b065c672e2
yuma kitamura <yuma.kitamura@smaregi.jp>
2019-09-05 11:51:13 +0900

    トップページのカイゼン

---
+++ docs/.vuepress/events.js
@@ -0,0 +1,56 @@
+export const Events = [
+    {
+        id: 1,
+        url: 'https://vuekansai.connpass.com/event/110542',
+        with_community: '',
+        created_at: '2018/12/20'
+    },
+    {
+        id: 2,
+        url: 'https://vuekansai.connpass.com/event/112900',
+        with_community: '',
+        created_at: '2019/01/19'
+    },
+    {
+        id: 3,
+        url: 'https://vuekansai.connpass.com/event/114795',
+        with_community: '',
+        created_at: '2019/02/28'
+    },
+    {
+        id: 4,
+        url: 'https://vuekansai.connpass.com/event/121581',
+        with_community: '',
+        created_at: '2019/03/22'
+    },
+    {
+        id: 5,
+        url: 'https://vuekansai.connpass.com/event/122664',
+        with_community: '',
+        created_at: '2019/04/25'
+    },
+    {
+        id: 6,
+        url: 'https://vuekansai.connpass.com/event/126754',
+        with_community: 'Monaca UG',
+        created_at: '2019/05/22'
+    },
+    {
+        id: 7,
+        url: 'https://vuekansai.connpass.com/event/127057',
+        with_community: '',
+        created_at: '2019/06/22'
+    },
+    {
+        id: 8,
+        url: 'https://vuekansai.connpass.com/event/136514',
+        with_community: '',
+        created_at: '2019/07/24'
+    },
+    {
+        id: 9,
+        url: 'https://vuekansai.connpass.com/event/137411',
+        with_community: 'FJUG',
+        created_at: '2019/08/21'
+    },
+]
--- docs/README.md
+++ docs/README.md
@@ -1,26 +1,6 @@
 ---
-home: true
+home: false
 footer: Created © 2019 jiyuujin LAB. All Rights Reserved.
 ---
 
-<TopFormat>
-<CardLink name="#9 (大阪)" date="08月21日" connpassLinkID="137411" image="logo" />
-<CardLink name="#8 (大阪)" date="07月24日" connpassLinkID="136514" image="logo" />
-<CardLink name="#7 (京都)" date="06月22日" connpassLinkID="127057" image="logo" />
-<CardLink name="#6 (大阪)" date="05月22日" connpassLinkID="126754" image="logo" />
-<CardLink name="#5 (京都)" date="04月25日" connpassLinkID="122664" image="logo" />
-<CardLink name="#4 (大阪)" date="03月22日" connpassLinkID="121581" image="logo" />
-<CardLink name="#3 (京都)" date="02月28日" connpassLinkID="114795" image="logo" />
-<CardLink name="#2 (大阪)" date="01月19日" connpassLinkID="112900" image="logo" />
-<CardLink name="#1 (京都)" date="12月20日" connpassLinkID="110542" image="logo" />
-</TopFormat>
-
-## グループの説明
-関西を拠点に(毎月京都と大阪で交互に)、Vue.js に興味のある方でしたらどなたでもお気軽にご参加ください。
-
-Twitterハッシュタグは #v_kansai を使っています。
-
-## 場所・スポンサー
-もしウチの会社でやっても良いよ、との声や、スポンサーになりたいなどの声がございましたら、
-
-ご気軽に @jiyuujin (https://twitter.com/jiyuujinlab) までご連絡ください。
+<EventCard/>
